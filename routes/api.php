e28e754fee380f5b184570acc1caaf85265a2a05
sakurai <sakurai_ch@yahoo.co.jp>
2021-05-14 23:56:24 +0900

    fix jwt

--- routes/api.php
+++ routes/api.php
@@ -14,7 +14,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/v1/store', [StoresController::class, 'index']);
 Route::get('/v1/store/{id}', [StoresController::class, 'show']);
 Route::post('/v1/user', [UsersController::class, 'post']);
-Route::group(['middleware' => 'auth:api'],function () {
+// Route::group(['middleware' => 'auth:api'],function () {
+Route::group(['middleware' => 'api'], function () {
   Route::get('/v1/user', [UsersController::class, 'get']);
   Route::post('/v1/logout', [LogoutController::class, 'post']);
 });
@@ -27,4 +28,3 @@ Route::get('/v1/reservation', [ReservationsController::class, 'get']);
 Route::patch('/v1/reservation', [ReservationsController::class, 'patch']);
 Route::get('/v1/area', [AreasController::class, 'get']);
 Route::get('/v1/genre', [GenresController::class, 'get']);
-
