7cd5216c83674269d2200e25dd77f518e920192e
sakurai <sakurai_ch@yahoo.co.jp>
2021-05-15 23:28:09 +0900

    fix jwt

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -42,7 +42,7 @@ class AuthController extends Controller
     public function me()
     {
         // return response()->json(auth()->user());
-        return response()->json(Auth::auth()->user());
+        return response()->json(Auth::user());
     }
 
     /**
