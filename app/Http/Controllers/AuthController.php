8a740db9a69ea059d0d275e30c38101ffd467384
sakurai <sakurai_ch@yahoo.co.jp>
2021-05-16 01:23:54 +0900

    fix jwt

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -81,14 +81,14 @@ class AuthController extends Controller
      */
     protected function respondWithToken($token)
     {
-        $items = request(['user_id']); //
+        $item = request(['user_id']); //
 
         return response()->json([
             'access_token' => $token,
             'token_type' => 'bearer',
             'expires_in' => auth('api')->factory()->getTTL() * 60,
             // 'expires_in' => Auth::factory()->getTTL() * 60,
-            'id' => $items->id, //
+            'id' => $item['user_id'], //
             'auth' => true, //
         ]);
     }
