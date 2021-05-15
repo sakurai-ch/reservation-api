a1a6bbbd4e4bc59794828d9d903a66fec2b2b245
sakurai <sakurai_ch@yahoo.co.jp>
2021-05-15 23:00:57 +0900

    fix jwt

--- config/cors.php
+++ config/cors.php
@@ -32,6 +32,7 @@ return [
     // 'max_age' => 0,
     'max_age' => false,
 
-    'supports_credentials' => false,
+    // 'supports_credentials' => false,
+    'supports_credentials' => true, //for jwt
 
 ];
