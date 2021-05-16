2c71c1c8a144075e2937d6af829067b301a00ae8
sakurai <sakurai_ch@yahoo.co.jp>
2021-05-16 13:04:55 +0900

    fix jwt

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,7 +1,6 @@
 <?php
 
-// namespace App\Models;
-namespace App;
+namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
