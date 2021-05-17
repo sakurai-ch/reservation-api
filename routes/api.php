0c3670512e8fc2be35e522e1f1a67b118a1d1ebc
sakurai <sakurai_ch@yahoo.co.jp>
2021-05-17 18:45:39 +0900

    fix jwt

--- routes/api.php
+++ routes/api.php
@@ -11,17 +11,20 @@ use App\Http\Controllers\GenresController;
 use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
-// Route::group( ['middleware' => 'api'], function () {
 
 Route::get('/v1/store', [StoresController::class, 'index']);
 Route::get('/v1/store/{id}', [StoresController::class, 'show']);
 Route::post('/v1/user', [UsersController::class, 'post']);
 
 Route::post('/v1/login', [AuthController::class, 'login']);
-Route::group(['middleware' => 'api'], function () {
-// Route::group(['middleware' => 'auth:api'], function () {
+// Route::group(['middleware' => 'api'], function () {
+Route::group(['middleware' => 'auth:api'], function () {
   // Route::get('/v1/user', [AuthController::class, 'me']);
   Route::post('/v1/logout', [AuthController::class, 'logout']);
+  Route::post('/v1/reservation', [ReservationsController::class, 'post']);
+  Route::delete('/v1/reservation', [ReservationsController::class, 'delete']);
+  Route::get('/v1/reservation', [ReservationsController::class, 'get']);
+  Route::patch('/v1/reservation', [ReservationsController::class, 'patch']);
 });
 
 // Route::get('/v1/user', [UsersController::class, 'get']);
@@ -30,10 +33,6 @@ Route::group(['middleware' => 'api'], function () {
 
 Route::post('/v1/favorite', [FavoritesController::class, 'post']);
 Route::delete('/v1/favorite', [FavoritesController::class, 'delete']);
-Route::post('/v1/reservation', [ReservationsController::class, 'post']);
-Route::delete('/v1/reservation', [ReservationsController::class, 'delete']);
-Route::get('/v1/reservation', [ReservationsController::class, 'get']);
-Route::patch('/v1/reservation', [ReservationsController::class, 'patch']);
 Route::get('/v1/area', [AreasController::class, 'get']);
 Route::get('/v1/genre', [GenresController::class, 'get']);
 
