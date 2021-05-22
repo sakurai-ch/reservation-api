8e1cbbda1ffbe7b60dd634d3d54f2f708ee82d47
sakurai <sakurai_ch@yahoo.co.jp>
2021-05-22 15:43:29 +0900

    add magager

--- routes/api.php
+++ routes/api.php
@@ -2,13 +2,12 @@
 
 use App\Http\Controllers\StoresController;
 use App\Http\Controllers\UsersController;
-use App\Http\Controllers\LoginController;
-use App\Http\Controllers\LogoutController;
 use App\Http\Controllers\FavoritesController;
 use App\Http\Controllers\ReservationsController;
 use App\Http\Controllers\AreasController;
 use App\Http\Controllers\GenresController;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ManagerController;
 use Illuminate\Support\Facades\Route;
 
 
@@ -31,4 +30,4 @@ Route::group(['middleware' => 'auth:api'], function () {
 });
 
 Route::patch('/v1/store/{id}', [StoresController::class, 'update']);
-Route::get('/v1/manager', [ManagersController::class, 'get']);
+Route::get('/v1/manager', [ManagerController::class, 'get']);
