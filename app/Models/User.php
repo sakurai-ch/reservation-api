5b692eba06d8b1da6cf646f2caae157319cf5ed2
sakurai <sakurai_ch@yahoo.co.jp>
2021-05-25 00:27:28 +0900

    fix manager

--- app/Models/User.php
+++ app/Models/User.php
@@ -39,7 +39,7 @@ class User extends Authenticatable implements JWTSubject
 
     public static function get_managers()
     {
-        $store_managers = Manager::all();
+        $store_managers = Manager::get();
         $param = user::where('manager', true)
         ->leftJoinSub($store_managers, 'store_managers', function ($join) {
             $join->on('user.id', '=', 'store_managers.user_id');
